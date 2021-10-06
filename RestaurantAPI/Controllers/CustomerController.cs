aa299e159a15abb8a08943c7569ff8fae447cebd
Ruwani Perera <perarar@angloeastern.com>
2021-10-06 12:07:18 +0800

    Add project files.

---
+++ RestaurantAPI/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly RestaurantDbContext _context;
+
+        public CustomerController(RestaurantDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _context.Customers.ToListAsync();
+        }
+    }
+}
